5ffd994d8e624909adf91fe28a12ccd1556171c1
Charlie Graham <graham7828@gmail.com>
2019-04-06 17:58:05 -0500

    communism?

--- scrapechan.py
+++ scrapechan.py
@@ -40,19 +40,20 @@ import sys
 
 
 # Blacklisted phrases or words
-# You may want separate lists for different boards
 #
-g_blacklist = ["bro ","seething ","apple ","install gentoo", "why haven","why doesn", "why aren","he doesnt","developer ", "based and","good afternoon","discord ","zoomers ","meme ","hey guys","smartphone"," phone","google","coding","memes ","browser","firefox","waterfox", "chrome ","chromium","sup ","rip ","gaym","android ","stickers ","game","web ","normie","itoddler","btfo "," shill"," sjw ", " interject ","one copy of","gaming","daily reminder","virtual reality"," vr "," brave", "he doesnt"," intel "," amd ","nvidia"," gpu"," 5g "," pajeet ", " burger ", " app", "freetard","windows 10","incel ","madmen","comfy","absolute state"," mac "," ios","reddit","what does g think","intellectual ","oh no no"]
+g_blacklist = ["bro ","seething ","apple ","install gentoo","absolute state","you may not like it but", "why havent","why doesnt", "why arent you","he doesnt","developer ", "based and","good afternoon","discord ","zoomers ","meme ","hey guys","smartphone"," phone","google","coding","memes ","browser","firefox","waterfox", "chrome ","chromium","sup "," moon","rip ","gaym","android ","stickers ","game","web ","normie","itoddler","btfo "," shill"," sjw ", " interject ","one copy of","gaming","daily reminder"," reminder ","virtual reality"," vr "," brave", "he doesnt"," intel "," amd ","nvidia"," gpu"," 5g "," pajeet ", " burger ", " app", "freetard","windows","incel ","madmen","comfy","absolute state"," mac "," ios","reddit","what does g think","intellectual ","oh no no"," cia ","youtube"," university ","student","threadripper"," rig ", "battlestation","prove me wrong"," youll ever need","gnome","instagram","facebook","twitter","tfw ","nothin personnel"]
 
+w_blacklist = ["phone", "mobile", "sailor moon", " car ", " cars ", "image modification", "one piece", "jojo", "dragon ball", "spice and wolf", "holo", "attack on titan", "read this before", "renders in shape", "request", "pokemon"]
+
+wg_blacklist = ["sticky", "cyberpunk", "political", "nazi","nude","fuckscape","sexy", " hot","game of thrones","quotes","wave ","birthday","mobile", "phone","punk"," girls ", " women ", " dudes ", " men ", " man ", "homescreen", "lockscreen", " apple ", " mac", "image modification", "40k"]
 
 
 # Default board is /g/ if no argument given
 #
-if (len(sys.argv) < 2):
-    url = "http://boards.4channel.org/g/catalog"
-else:
-    url = "http://boards.4channel.org/" + sys.argv[1]  + "/catalog"
-
+board = 'g'
+if len(sys.argv) > 1:
+    board = sys.argv[1]
+url = "http://boards.4channel.org/" + board + "/catalog"
 
 
 # Get html content
@@ -68,36 +69,57 @@ thread_list = re.findall(r'teaser":"(.*?)"',content.text)
 
 
 # Make all text lowercase, remove special characters, and find percentage of bad threads
-# Bad if:
-#       Title is less than 5 words
-#       Starts with greentext
-#       Word from blacklist matches
 #
 special_chars = ["/","\\","-","\'","\"","?","<",";",":",".",",","!","@","#","$","%","^","&","*","(",")","0","1","2","3","4","5","6","7","8","9"]
 greentext = "&gt;" #html for '>'
 total_threads = 151
 bad_thread_count = 0
-for title in thread_list:
-    if len(title.split()) < 5:
-        bad_thread_count += 1
-        continue
-    elif (title[0:4] == greentext): # If title starts with greentext
-        bad_thread_count += 1
-        continue
-    else:
-        for char in special_chars:
-            title = title.replace(char,"")
-        for phrase in g_blacklist:
+
+if board == 'g':
+    for title in thread_list:
+     if  len(title.split()) < 5:
+         bad_thread_count += 1
+         continue
+     elif (title[0:4] == greentext): # If title starts with greentext
+         bad_thread_count += 1
+         continue
+     else:
+         for char in special_chars:
+             title = title.replace(char,"")
+         for phrase in g_blacklist:
+             if phrase in title.lower():
+                 bad_thread_count += 1
+                 break
+
+elif board == 'w':
+    for title in thread_list:
+         for char in special_chars:
+             title = title.replace(char,"")
+         for phrase in w_blacklist:
             if phrase in title.lower():
                 bad_thread_count += 1
-                '''
-                #Uncomment to see title of bad thread and phrase that trigged it
-                print()
-                print("bad title:",title)
-                print("matching phrase:",phrase)
-                print()
-                '''
                 break
 
-percentage = round((bad_thread_count/total_threads)*100)
+elif board == 'wg':
+    for title in thread_list:
+         for char in special_chars:
+             title = title.replace(char,"")
+         for phrase in wg_blacklist:
+            if phrase in title.lower():
+                bad_thread_count += 1
+                break
+
+
+percentage = round(( (total_threads-bad_thread_count) / total_threads)*100)
 print(str(percentage)+"%")
+
+
+
+
+
+
+
+
+
+
+
